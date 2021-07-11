bdef52473980f4c3ecef2e8316016589dc89c895
PRIYANSHU DAS <ask.priyanshu@gmail.com>
2021-07-12 01:50:18 +0530

    Added basic bot code

--- src/main.py
+++ src/main.py
@@ -1,10 +1,19 @@
-import discord
+import json
+import requests
 import os
+import discord
 from dotenv import load_dotenv
-
 load_dotenv()
 
+prefix = "$"
 client = discord.Client()
+
+
+def get_quote():
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " -" + json_data[0]['a']
+    return(quote)
 
 
 @client.event
@@ -14,13 +23,28 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    print(message)
+    print(message.channel)
+    print(message.content)
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    else:
+        await parse_commands(command=message.content, channel=message.channel)
 
 
-# print(os.getenv('TOKEN'))
+async def parse_commands(command, channel):
+
+    if "prefix" in command:
+        change_prefix(new_prefix=command[-1])
+    if command.startswith(prefix+'inspire'):
+        quote = get_quote()
+        await channel.send(quote)
+
+
+def change_prefix(new_prefix):
+    global prefix
+    prefix = new_prefix
+
 
 client.run(os.getenv('TOKEN'))
